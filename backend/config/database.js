904b9cd732b5374b6fb4315ef6be369110fa4a64
vikash380771 <vb380771@gmail.com>
2026-03-12 09:47:34 +0530

    add config file

---
+++ backend/config/database.js
@@ -0,0 +1,29 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+let sequelize;
+
+
+if (process.env.DB_DIALECT === 'sqlite') {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: process.env.DB_STORAGE || './database.sqlite',
+    logging: false
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'mysql',
+      logging: false
+    }
+  );
+}
+
+module.exports = sequelize;
